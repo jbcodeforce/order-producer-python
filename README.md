511789c5698f6eabbc6422987a3e3b58ad6fcac4
Jerome Boyer <boyerje@us.ibm.com>
2019-07-19 12:54:21 -0700

    Add env for broker

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# restonapython
+# Kafka producer with flask
+
+To build with s2i
+
+```
+s2i build --copy .  centos/python-36-centos7 ibmcase/orderproducer
+```
+
+To run
+
+```
+docker run -p 8080:8080 ibmcase/orderproducer
+
+```
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,37 @@
 from flask import Flask
+import os
+
+try:
+    KAFKA_BROKERS = os.environ['KAFKA_BROKERS']
+except KeyError:
+    # KAFKA_BROKERS = "kafka03-prod02.messagehub.services.us-south.bluemix.net:9093,kafka01-prod02.messagehub.services.us-south.bluemix.net:9093,kafka02-prod02.messagehub.services.us-south.bluemix.net:9093,kafka05-prod02.messagehub.services.us-south.bluemix.net:9093,kafka04-prod02.messagehub.services.us-south.bluemix.net:9093"
+    KAFKA_BROKERS = "localhost:9092"
+
 application = Flask(__name__)
+
+def createOrder(id):
+    print('Create order')
+    data = {"orderID": id, 
+        "productID": "FreshFoodItg", 
+        "customerID": "Customer000",
+        "quantity": 180, 
+        "pickupAddress": {"street": "main","city": "Oakland","country":"USA","state":"CA","zipcode": "95000"},
+        "destinationAddress": {"street": "bstreet","city": "Beijing","country":"China","state":"NE","zipcode": "09000"},
+        "pickupDate": "2019-05-25",
+        "expectedDeliveryDate": "2019-06-25"}
+    containerEvent = {"orderID": id,"timestamp": int(time.time()),"type":"OrderCreated","payload": data}
+    return containerEvent
+
 
 @application.route("/")
 def hello():
-    return "Jbcodeforce on Openshift Hello to you!"
+    print(KAFKA_BROKERS)
+    return "Jbcodeforce on Openshift Hello to you! v01"
+
+@application.route("/order")
+def getOrder():
+    return createOrder("10")
 
 if __name__ == "__main__":
     application.run()
+    
